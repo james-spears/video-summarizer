84a63d755e8b093c72d8e142748d796a309c62f9
James Spears <james.w.spears@gmail.com>
2023-07-09 10:58:32 -0400

    add file drop

--- src/static/main.ts
+++ src/static/main.ts
@@ -14,10 +14,50 @@ document.addEventListener("alpine:init", () => {
         this.show = true;
       });
       this.$refs.input.onchange = async () => {
+        console.log('change');
         Array.from<File>(
           (this.$refs.input as HTMLInputElement).files || []
         ).forEach((file) => this.videos.push({ file, summary: "" }));
       };
+    },
+    drop(event: DragEvent, el: HTMLElement) {
+      // console.log("File(s) dropped");
+      el.classList.remove('!text-gray-500');
+      el.classList.remove('!border-gray-500');
+      el.classList.add('border-brand-500');
+      // Prevent default behavior (Prevent file from being opened)
+      event.preventDefault();
+    
+      if (event.dataTransfer?.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        [...event.dataTransfer.items].forEach((item) => {
+          // If dropped items aren't files, reject them
+          if (item.kind === "file") {
+            const file = item.getAsFile();
+            this.videos.push({ file, summary: "" });
+          }
+        });
+      } else if(event.dataTransfer?.files) {
+        // Use DataTransfer interface to access the file(s)
+        [...event.dataTransfer.files].forEach((file) => {
+          this.videos.push({ file, summary: "" });
+        });
+      }
+    },
+    dragover(event: DragEvent, el: HTMLElement) {
+      // console.log("File(s) in drop zone");
+      el.classList.add('!text-gray-500');
+      el.classList.add('!border-gray-500');
+    
+      // Prevent default behavior (Prevent file from being opened)
+      event.preventDefault();
+    },
+    dragleave(event: DragEvent, el: HTMLElement) {
+      // console.log("File(s) nolonger in drop zone");
+      el.classList.remove('!text-gray-500');
+      el.classList.remove('!border-gray-500');
+      // Prevent default behavior (Prevent file from being opened)
+      event.preventDefault();
     },
     submitForm() {
       this.summary = [];
